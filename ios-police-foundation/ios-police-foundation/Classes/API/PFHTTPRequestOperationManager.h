53ec1bd8a9c2b395b2c76920cdd185bc1f2ce7a0
Aaron Connolly <aaron.connolly@live.com>
2014-05-18 11:45:51 -0700

    IPF-1: COMPLETED
    - reorganized classes and other files in project structure
    - finished basic API class to request data from WordPress REST API

---
+++ ios-police-foundation/ios-police-foundation/Classes/API/PFHTTPRequestOperationManager.h
@@ -0,0 +1,28 @@
+//
+//  PFRequestOperationManager.h
+//  ios-police-foundation
+//
+//  Created by Aaron Connolly on 5/17/14.
+//  Copyright (c) 2014 Police Foundation. All rights reserved.
+//
+
+#import "AFHTTPRequestOperationManager.h"
+
+@interface PFHTTPRequestOperationManager : AFHTTPRequestOperationManager
+
++ (id)sharedManager;
+
+- (void)getCategoriesWithParameters:(NSDictionary *)parameters
+                            successBlock:(void (^)(AFHTTPRequestOperation * operation, id responseObject))successBlock
+                            failureBlock:(void (^)(AFHTTPRequestOperation * operation, NSError * error))failureBlock;
+
+- (void)getPostsWithParameters:(NSDictionary *)parameters
+                  successBlock:(void (^)(AFHTTPRequestOperation * operation, id responseObject))successBlock
+                  failureBlock:(void (^)(AFHTTPRequestOperation * operation, NSError * error))failureBlock;
+
+- (void)getPostWithId:(NSString*)postId
+           parameters:(NSDictionary *)parameters
+         successBlock:(void (^)(AFHTTPRequestOperation * operation, id responseObject))successBlock
+         failureBlock:(void (^)(AFHTTPRequestOperation * operation, NSError * error))failureBlock;
+
+@end
