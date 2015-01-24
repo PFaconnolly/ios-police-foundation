c44a9b5d717e31defce2370fe05c932a997a304a
Aaron Connolly <aaron.connolly@live.com>
2015-01-24 10:18:37 -0800

    IPF-77: Added section headers to research screen for most recent X articles.

---
+++ ios-police-foundation/ios-police-foundation/Classes/Views/PFResearchHeaderCollectionReusableView.m
@@ -0,0 +1,13 @@
+//
+//  PFResearchHeaderCollectionReusableView.m
+//  ios-police-foundation
+//
+//  Created by Aaron Connolly on 1/24/15.
+//  Copyright (c) 2015 Police Foundation. All rights reserved.
+//
+
+#import "PFResearchHeaderCollectionReusableView.h"
+
+@implementation PFResearchHeaderCollectionReusableView
+
+@end
